a2e814bbad2dbe24e1b5bf648b848c4a7b43cb40
unknown <gustavohenriquearagaosilva@gmail.com>
2023-09-09 11:19:02 -0300

    Corrigindo o exercício Q10 da Lista 05

--- Lista 05/q10.js
+++ Lista 05/q10.js
@@ -5,7 +5,7 @@ const lista = [1,2,3]
 const replicarElementosLista = (reps) => (lista) => lista
     .map((numero) => numero.toString()) // Transfomar a lista de números em lista de strings
     .map((numeroStr) => numeroStr.repeat(reps)) // Repetir essas strings em "reps" vezes
-    .reduce(somar,'') // Concatenar todas as strings dessa lista -> Retorna uma String
+    .reduce((x, acc) => x + acc,'') // Concatenar todas as strings dessa lista -> Retorna uma String
     .split('') // "Splitamos" essa string em uma lista de strings -> Retorna uma Array
 
 console.log(replicarElementosLista(2)(lista))
