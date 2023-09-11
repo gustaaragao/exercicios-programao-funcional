9bde9d20b954bf4ba56fe6af2a674946c2292cdc
unknown <gustavohenriquearagaosilva@gmail.com>
2023-09-11 07:35:52 -0300

    Finalizando Lista 04.

---
+++ Lista 04 (Recursividade)/q08.js
@@ -0,0 +1,4 @@
+/*
+Q8. Máximo Divisor Comum (MDC) entre dois números inteiros positivos, n e m. 
+Naturalmente, você não deve utilizar operadores de divisão da linguagem.
+*/
---
+++ Lista 04 (Recursividade)/q09.js
@@ -0,0 +1,4 @@
+/*
+Q9. Mínimo Múltiplo Comum (MMC) entre dois inteiros positivos fornecidos, n e m. 
+Naturalmente, você não deve utilizar operadores de divisão da linguagem.
+*/
