e029cb2d1f75a21baa19582efc6944bff8005fe1
unknown <gustavohenriquearagaosilva@gmail.com>
2023-09-13 15:43:23 -0300

    Finalizando a Lista 07.

--- Lista 07 (Recursividade em listas)/q08.js
+++ Lista 07 (Recursividade em listas)/q08.js
@@ -7,8 +7,19 @@ Exemplos: [-4,0,3,7,11].buscabin(7) ---> 3; [-4,0,3,7,11].buscabin(1) ---> -1 (o
 // Lembrete: O algoritmo da Busca Binária pressupõe que a Lista está ordenada. 
 // Caso não esteja, podemos usar a função qSort em q07.js (usar o algoritmo Quick sort).
 
-const buscaBinaria = (lista, item) => {
-
+const buscaBinaria = (lista, alvo) => {
+    const buscaBinariaAux = (inicio, fim) => {
+        const meio = Math.floor((inicio + fim) / 2)
+        const valorMeio = lista[meio]
+        
+        if (inicio > fim) return -1; // Elemento não encontrado
+        else if (valorMeio === alvo) return meio; // Elemento encontrado
+        else if (alvo < valorMeio) return buscaBinariaAux(inicio, meio - 1); // Buscar metade esquerda
+        else return buscaBinariaAux(meio + 1, fim); // Buscar metade direita
+    }
+    return buscaBinariaAux(0, lista.length - 1);
 }
 
+console.log(buscaBinaria([-4,0,3,4,7], 5)) // -1
+console.log(buscaBinaria([-4,0,3,4,7], 0)) // 1
 console.log(buscaBinaria([-4,0,3,4,7], 7)) // 4
